c178db07906fba097aacafb3dad2f5cafd691611
KingaKoziel <koziel.kinga4@gmail.com>
2021-04-15 09:54:10 +0200

    a

---
+++ Jendorozce/str1/strona.php
@@ -0,0 +1,79 @@
+<html>
+<head>
+    <meta charset="UTF-8"/>
+<link rel="stylesheet" href="style.css">
+        
+</head>
+    <body>
+     
+         <a href="https://github.com/KingaKoziel/.php">github</a>
+  
+        <div class="nav">
+      <a href="../../index.php">Strona główna</a>
+      <a href="../index.html">Cofnij</a>
+      <div class="container">
+        <div class="p1">
+
+        <?php
+        require_once("../../connect.php");
+        $sql = "SELECT * FROM Auta";
+        
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+              }
+        
+            $result = mysqli_query($conn, $sql);
+            if ( $result) {
+                 echo "<br>";
+             } else {
+               echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+             }
+        
+            echo("<table border='1'>");
+            echo("<th>ID</th><th>Auto</th>");
+                while($row = mysqli_fetch_assoc($result)) {
+                    echo("<tr>");
+                    echo("<td>".$row['id']."</td><td>".$row['Auto']."</td>");
+                    echo("</tr>");
+                };
+            echo("</table>");
+            echo ("<br>");
+
+        ?>
+        </div>
+        <div class="p2">2</div>
+        <div class="p3">
+        <?php
+        require_once("../../connect.php");
+        $sql = "SELECT * FROM Kolor";
+        
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+              }
+        
+            $result = mysqli_query($conn, $sql);
+            if ( $result) {
+                 echo "<br>";
+             } else {
+               echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+             }
+        
+            echo("<table border='1'>");
+            echo("<th>ID</th><th>Kolor</th>");
+                while($row = mysqli_fetch_assoc($result)) {
+                    echo("<tr>");
+                    echo("<td>".$row['id']."</td><td>".$row['Kolor']."</td>");
+                    echo("</tr>");
+                };
+            echo("</table>");
+            echo ("<br>");
+
+        ?>
+        </div>
+
+        <div class="p4">
+       
+        <div class="p5">5</div>      
+    </div>
+    </body>
+</html>
