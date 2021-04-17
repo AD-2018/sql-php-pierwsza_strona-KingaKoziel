cb66c0b3147fe52263e522c8b193616549bade75
KingaKoziel <koziel.kinga4@gmail.com>
2021-04-17 14:25:38 +0200

    a

--- Jendorozce/str1/strona.php
+++ Jendorozce/str1/strona.php
@@ -5,18 +5,18 @@
         
 </head>
     <body>
-     
-         <a href="https://github.com/KingaKoziel/.php">github</a>
-  
-        <div class="nav">
-      <a href="../../index.php">Strona główna</a>
-      <a href="../index.html">Cofnij</a>
-      <div class="container">
+    <div class="container">
+    <div class="nav">
+         <a class= "link" href="https://github.com/AD-2018/sql-php-pierwsza_strona-KingaKoziel.git">github</a>
+      <a class= "link" href="../../index.php">Strona główna</a>
+      <a class= "link" href="../index.html">Cofnij</a>
+</div>
+
         <div class="p1">
 
         <?php
         require_once("../../connect.php");
-        $sql = "SELECT * FROM Auta";
+        $sql = "SELECT * FROM Auto";
         
             if ($conn->connect_error) {
                 die("Connection failed: " . $conn->connect_error);
@@ -33,7 +33,7 @@
             echo("<th>ID</th><th>Auto</th>");
                 while($row = mysqli_fetch_assoc($result)) {
                     echo("<tr>");
-                    echo("<td>".$row['id']."</td><td>".$row['Auto']."</td>");
+                    echo("<td>".$row['id_auto']."</td><td>".$row['Auto']."</td>");
                     echo("</tr>");
                 };
             echo("</table>");
@@ -45,7 +45,7 @@
         <div class="p3">
         <?php
         require_once("../../connect.php");
-        $sql = "SELECT * FROM Kolor";
+        $sql = "SELECT * FROM Mechanik";
         
             if ($conn->connect_error) {
                 die("Connection failed: " . $conn->connect_error);
@@ -59,10 +59,10 @@
              }
         
             echo("<table border='1'>");
-            echo("<th>ID</th><th>Kolor</th>");
+            echo("<th>ID</th><th>Machenik</th>");
                 while($row = mysqli_fetch_assoc($result)) {
                     echo("<tr>");
-                    echo("<td>".$row['id']."</td><td>".$row['Kolor']."</td>");
+                    echo("<td>".$row['id_machanik']."</td><td>".$row['Mechanik']."</td>");
                     echo("</tr>");
                 };
             echo("</table>");
@@ -71,8 +71,23 @@
         ?>
         </div>
 
-          <div class="p4">4</div>
-       
+          <div class="p4"> 
+        <?php
+        require_once("../../connect.php");
+        $sql = "select * from Auto, Mechanik where id_auto=id_mechanik"; 
+echo("<h3>Kolor Auta</h3>"); 
+$result = mysqli_query($conn, $sql);  
+echo('<table border="1" class="tabela"'); 
+echo('<tr><th>Auto</th><th>Mechanik</th><th>ID</th></tr>');
+while($row=mysqli_fetch_assoc($result)){ 
+  echo("<tr>");     
+  echo("<tr>");     
+  echo('<td>'.$row['Auto'].'</td><td>'.$row['Mechanik'].'</td><td>'.$row['id_auto'].'</td>');     
+  echo("<tr>"); 
+  echo("</tr>"); } 
+echo('</table>'); 
+              ?>
+       </div>  
         <div class="p5">5</div>      
     </div>
     </body>
